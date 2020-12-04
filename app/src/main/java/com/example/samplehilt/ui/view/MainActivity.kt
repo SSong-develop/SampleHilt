5626de633cb0b06192f68b61fa0e7e73c978358a
SSong-develop <thdgnsrl123@naver.com>
2020-12-04 17:50:53 +0900

    initial commit

---
+++ app/src/main/java/com/example/samplehilt/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.samplehilt.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.samplehilt.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
